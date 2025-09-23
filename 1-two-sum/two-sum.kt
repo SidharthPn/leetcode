86669102f3a3479ae957988c507c125311bb1883
SIDHARTH PN <sid7040@pg.cusat.ac.in>
2025-09-23 10:13:25 +0530

    Time: 34 ms (16.97%) | Memory: 48.8 MB (13.75%) - LeetSync

---
+++ 1-two-sum/two-sum.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+               for (i in nums.indices){
+                for (j in i+1 until nums.size){
+               if(target == nums[i] + nums[j]){
+                return intArrayOf(i,j)
+            }
+        } 
+    }
+     return intArrayOf() 
+}
+}
