7fc116e5e3ddda525490693015914591757967a5
SIDHARTH PN <sid7040@pg.cusat.ac.in>
2025-09-28 23:28:24 +0530

    Add solution for 1550. Three Consecutive Odds

---
+++ ThreeConsecutiveOdds.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun threeConsecutiveOdds(arr: IntArray): Boolean {
+        var count = 0 
+        for(num in arr){
+            if( num % 2 != 0){
+                count++
+                if(count==3) return true
+            } else {
+                count = 0
+            }
+        }
+        return false
+    }
+}
