ce3451550dc11c215c14048ba88b8ca2c67780a5
SIDHARTH PN <sid7040@pg.cusat.ac.in>
2025-09-28 22:53:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (95.83%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun threeConsecutiveOdds(arr: IntArray): Boolean {
+        var count = 0 
+        
+        for(num in arr){
+        if( num % 2 != 0){
+            count++
+            if(count==3) return true
+        } else {
+            count = 0
+        }
+        }
+        return false
+    }
+}
