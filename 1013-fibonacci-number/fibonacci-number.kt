1f43bd4a063a9141ce1f6a47694f5fd8f5eef3bb
SIDHARTH PN <sid7040@pg.cusat.ac.in>
2025-10-01 20:13:38 +0530

    Time: 95 ms (5.74%) | Memory: 39.2 MB (58.37%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun fib(n: Int): Int {
+        if( n == 0) return 0
+        if( n == 1) return 1
+
+        var a = 0
+        var b = 1
+        var result = 0
+        for (i in 2..n){
+            result = a + b
+             a = b
+             b = result
+        }
+        return result
+    }
+}
